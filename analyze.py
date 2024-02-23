8173d1e573f542774328014ee1c274e7cc4814ff
Radomír Bosák <radomir.bosak@gmail.com>
2024-02-23 14:59:00 +0900

    Analyze most common readings for each radical

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import warnings
-from collections import Counter
+from collections import Counter, defaultdict
 
 import numpy as np
 
@@ -35,8 +35,59 @@ def print_most_common_readings():
         print(reading, "\t", frequency)
 
 
+def print_most_common_radicals():
+    radical_counter = Counter([kanji.radical for kanji in allk])
+
+    for radical, frequency in radical_counter.most_common(20):
+        print(radical, "\t", frequency)
+
+
+def get_radical_counters():
+    radical_counters = defaultdict(Counter)
+    for kanji in allk:
+        readings = [katakana_to_hiragana(on) for on in kanji.onyomi_text]
+        radical_counters[kanji.radical].update(readings)
+    return radical_counters
+
+
+def most_common_reading_for_radical():
+    radical_counters = get_radical_counters()
+    radical_influence = get_radical_influence()
+
+    for radical, influence in radical_influence[:20]:
+        counter = radical_counters[radical]
+        readingpart = ""
+        for reading, frequency in counter.most_common(3):
+            readingpart += f" {reading}({frequency})\t"
+        radicalpart = f"{radical}({influence})"
+        print(radicalpart, readingpart)
+
+
+def get_radical_influence():
+    """
+    Return (radical, frequency) pairs, e.g.
+    {'水': 118, '人': 98, '手': 95, ...}
+    """
+    all_radicals = set(kanji.radical for kanji in allk)
+    radical_influence = {
+        radical: len([1 for kanji in allk if kanji.radical == radical])
+        for radical in all_radicals
+    }
+    return sorted(radical_influence.items(), key=lambda x: -x[1])
+
+
+def print_most_influental_radicals():
+
+    srads = dict(get_radical_influence())
+    for radical, influence in list(srads.items())[:20]:
+        print(radical, influence)
+
+
 def main():
-    print_most_common_readings()
+    # print_most_common_readings()
+    # print_most_common_radicals()
+    most_common_reading_for_radical()
+    # print_most_influental_radicals()
 
 
 if __name__ == "__main__":
